9bb2b60acad344dae6782bb03ddb616fff696ab7
Akhlaque-1 <akhlaqueahmed397@gmail.com>
2026-04-24 13:04:59 +0500

    updated class-01

--- backend/nodejs-class-01/app.js
+++ backend/nodejs-class-01/app.js
@@ -34,3 +34,52 @@ console.log("Server running at http://localhost:3000");
 const fs = require('fs');
 fs.writeFileSync('test.txt', 'Hello from Node.js');
 
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+  console.log("req ==>", req.url);
+
+  // Home Route
+  if (req.url === "/") {
+    res.writeHead(200, { "Content-Type": "text/plain" });
+    res.end("You are welcome to our server!");
+  }
+
+  // Products Route
+  else if (req.url.startsWith("/products")) {
+    res.writeHead(200, { "Content-Type": "application/json" });
+
+    const products = {
+      Product: [
+        { id: 1, title: "product 1" },
+        { id: 2, title: "product 2" }
+      ]
+    };
+
+    res.end(JSON.stringify(products));
+  }
+
+  // Todos Route
+  else if (req.url.startsWith("/todos")) {
+    res.writeHead(200, { "Content-Type": "application/json" });
+
+    const todos = {
+      todos: [
+        { id: 1, title: "todo 1" },
+        { id: 2, title: "todo 2" }
+      ]
+    };
+
+    res.end(JSON.stringify(todos));
+  }
+
+  // 404 Route
+  else {
+    res.writeHead(404, { "Content-Type": "text/plain" });
+    res.end("This item is not available!");
+  }
+});
+
+server.listen(4000, "127.0.0.1", () => {
+  console.log("Server is up and running on PORT: 4000");
+});
