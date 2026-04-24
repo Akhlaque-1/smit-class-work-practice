b340d68eaae499cc821df02192764e9e8d68d31f
Akhlaque-1 <akhlaqueahmed397@gmail.com>
2026-04-24 15:11:53 +0500

    class 02 code

---
+++ backend/nodejs-class-02/app.js
@@ -0,0 +1,41 @@
+const express = require("express");
+
+const app = express();
+const PORT = 4000;
+
+// Middleware (future use ke liye)
+app.use(express.json());
+
+// Home route
+app.get("/", (req, res) => {
+  res.send("🚀 Welcome to My Express API");
+});
+
+// All products
+app.get("/products", (req, res) => {
+  const products = [
+    { id: 1, name: "Shirt", price: 1200 },
+    { id: 2, name: "Jeans", price: 2500 },
+    { id: 3, name: "Shoes", price: 5000 }
+  ];
+
+  res.json(products);
+});
+
+// Single product (practice route)
+app.get("/products/:id", (req, res) => {
+  const id = req.params.id;
+
+  res.json({
+    message: "Single product fetched",
+    product: {
+      id: id,
+      name: `Product ${id}`
+    }
+  });
+});
+
+// Server start
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
